ce1cb945658260a4cd6690184f33923582b34d70
pz <1401499095@qq.com>
2019-11-18 00:22:14 +0800

    pz

--- src/main/java/com/fh/shop/admin/po/pz/book.java
+++ src/main/java/com/fh/shop/admin/po/pz/book.java
@@ -3,6 +3,17 @@ package com.fh.shop.admin.po.pz;
 public class book {
     private String name;
 
+    private String age;
+
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
