c001e6914cbca5a2ff5076d9f2b1e738c1c670ee
liu-7299 <57224236+liu-7299@users.noreply.github.com>
2019-11-17 18:34:15 +0800

    init liuxy

---
+++ src/main/java/com/fh/shop/admin/po/liuxy/Book.java
@@ -0,0 +1,16 @@
+package com.fh.shop.admin.po.liuxy;
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+
+    private String name = "liuxy";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
