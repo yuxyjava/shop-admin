b1d75ea98f1f58c47f7da22cc2f456cc3c3392b5
wangh <2394946889@qq.com>
2019-11-17 23:35:12 +0800

    tijiao two

--- src/main/java/com/fh/shop/admin/po/wanghao/Test.java
+++ src/main/java/com/fh/shop/admin/po/wanghao/Test.java
@@ -1,4 +1,16 @@
 package com.fh.shop.admin.po.wanghao;
 
-public class Test {
+import java.io.Serializable;
+
+public class Test implements Serializable {
+
+    private String git;
+
+    public String getGit() {
+        return git;
+    }
+
+    public void setGit(String git) {
+        this.git = git;
+    }
 }
