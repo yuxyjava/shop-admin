c4dd1549a44b4ba7bfb6a25556d9bcb45ae7928b
Ming <1664046428wsp>
2019-11-17 19:07:30 +0800

    wangsptijiao-2

---
+++ src/main/java/com/fh/shop/admin/po/wangsp2/Book.java
@@ -0,0 +1,15 @@
+package com.fh.shop.admin.po.wangsp2;
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
