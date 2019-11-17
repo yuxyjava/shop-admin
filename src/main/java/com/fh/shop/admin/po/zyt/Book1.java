72f717018e2dfe47db3512120342ecc8f470217c
zyt-git <2381650359@qq.com>
2019-11-17 23:57:20 +0800

    zyt

---
+++ src/main/java/com/fh/shop/admin/po/zyt/Book1.java
@@ -0,0 +1,14 @@
+package com.fh.shop.admin.po.zyt;
+
+public class Book1 {
+
+    private String price;
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
