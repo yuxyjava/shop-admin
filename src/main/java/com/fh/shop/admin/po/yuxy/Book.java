7827fbc97ac610d8e5fda48d65b09a17bc25c3df
wangpengfei <wangpengfei@126.com>
2019-11-17 18:41:13 +0800

    wangpengftijiao

---
+++ src/main/java/com/fh/shop/admin/po/yuxy/Book.java
@@ -0,0 +1,14 @@
+package com.fh.shop.admin.po.yuxy;
+
+public class Book {
+
+    private String price;
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
