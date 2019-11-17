413948b1bd2006c26be4ce8e1b234545a43e889c
yourname <you@example.com>
2019-11-17 20:30:38 +0800

    zjh

---
+++ src/main/java/com/fh/shop/admin/po/zhangjiahui/Book.java
@@ -0,0 +1,15 @@
+package com.fh.shop.admin.po.zhangjiahui;
+
+public class Book {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
